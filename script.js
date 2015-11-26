e7100c0feb9ff64e1ffa3ecc4e03e022b8121c0c
stephthi <nstephanie2@gmail.com>
2015-11-26 15:08:47 -0500

    Able to input into squares

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-$(document).on(‘ready’, function() {
+$(document).on('ready', function() {
 	var player1 = "player1";
 	var player2 = "player2";
+	var turn = player1;
 	var winningCombos =[[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9]];
 
 var turns = function() {
@@ -9,7 +10,17 @@ var turns = function() {
     } else {
       turn = "player1"
     }
+    $(this).text('x');
   }
+
+$('td').on('click', turns);
+
+
+var winner = function() {
+	$('td').off("click");
+  window.alert(turn + " winner");
+
+ }
 
 
 
