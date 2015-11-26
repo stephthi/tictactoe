0d677b14a12b514c1922d093980a59534352144b
stephthi <nstephanie2@gmail.com>
2015-11-26 14:06:10 -0500

    Created vars for turns, winning combos and players

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).on(‘ready’, function() {
+	var player1 = "player1";
+	var player2 = "player2";
+	var winningCombos =[[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9]];
+
+var turns = function() {
+    if (turn == "player1") {
+      turn = "player2"
+    } else {
+      turn = "player1"
+    }
+  }
+
+
 
 });
